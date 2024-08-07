1f584ed702e0122c481a5d47d2054fcb8b811534
David Damjanovic <damjanovic.david@gmail.com>
2024-08-07 16:35:48 +0200

    Add Women's page

--- src/Pages/Women.jsx
+++ src/Pages/Women.jsx
@@ -1,9 +1,27 @@
-import React from "react";
+import React, { useState, useEffect }from "react";
+import SneakerCard from "../Components/SneakerCard";
+import sneakersData from "../products-women.json";
 
 function Women() {
+    const [sneakers, setSneakers] = useState([]);
+
+    useEffect(() => {
+        setSneakers(sneakersData);
+    }, []);
+
     return (
-        <div>
-            <p>This is a Women page.</p>
+        <div className="women-page">
+            <div className="card-container">
+            {sneakers.map((sneaker) => (
+                <SneakerCard
+                key={sneaker.id}
+                name={sneaker.name}
+                price={sneaker.price}
+                image={sneaker.image}
+                brand={sneaker.brand}
+                />
+                ))}
+            </div>
         </div>
     )
 }
