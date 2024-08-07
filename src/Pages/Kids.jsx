037e655fac974659c05f79c698d35999d56024cf
David Damjanovic <damjanovic.david@gmail.com>
2024-08-07 17:12:52 +0200

    Add Kid's page

--- src/Pages/Kids.jsx
+++ src/Pages/Kids.jsx
@@ -1,9 +1,27 @@
-import React from "react";
+import React, { useEffect, useState} from "react";
+import sneakersData from "../products-kids.json";
+import SneakerCard from "../Components/SneakerCard";
 
 function Kids() {
+    const [sneakers, setSneakers] = useState([]);
+
+    useEffect(() => {
+        setSneakers(sneakersData);
+    }, []);
+
     return (
-        <div>
-            <p>This is a Kids page.</p>
+        <div className="kids-page">
+            <div className="card-container">
+            {sneakers.map((sneaker) => (
+                <SneakerCard
+                key={sneaker.id}
+                name={sneaker.name}
+                price={sneaker.price}
+                image={sneaker.image}
+                brand={sneaker.brand}
+                />
+                ))}
+            </div>
         </div>
     )
 }
